04976f31e13746b2270966f9065a35d91cd67308
EJ <eobarobe@uvm.edu>
2021-05-26 19:12:24 -0400

    one more for good luck

---
+++ src/main/java/com/revature/project1/webServices/DependencyLoaderListener.java
@@ -0,0 +1,35 @@
+package com.revature.project1.webServices;
+
+import com.revature.project1.daos.UserDAO;
+import com.revature.project1.services.UserService;
+import com.revature.project1.webServices.servlet.AuthServlet;
+import com.revature.project1.webServices.servlet.UserServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+// Tied to startup and shutdown of Tomcat. Implement the SCL and put your logic in
+// the overridden methods. Make sure you inform Tomcat of this class by including
+// it in your deployment descriptor(web.xml) under the listener tag
+public class DependencyLoaderListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        UserDAO userDAO = new UserDAO();
+        UserService userService = new UserService(userDAO);
+
+        AuthServlet authServlet = new AuthServlet(userService);
+        UserServlet userServlet = new UserServlet(userService);
+
+        ServletContext context = servletContextEvent.getServletContext();
+        context.addServlet("AuthServlet",authServlet).addMapping("/authentication");
+        context.addServlet("UserServlet",authServlet).addMapping("/users/*");
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
